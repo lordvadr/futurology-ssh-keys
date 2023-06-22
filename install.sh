a3b8527cf991e19f25660046e4db39550dacf4c1
lordvadr <lordvadr@futurology.social>
2023-06-22 10:46:40 -0500

    Moves users to directory, adds install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+log() {
+	>&2 echo "${@:-UNKNOWN MESSAGE}"
+}
+
+die() {
+	log "FATAL: ${@:-UNKNOWN FATAL ERROR}"
+	exit 1
+}
+
+[ -z "${DEBUG+x}" ] || { >&2 echo "DEBUG environment variable is set. Enabling debugging output."; set -x; }
+
+path="$(realpath "${0}")" || die "Could not determine path of executable."
+
+[ -d "${path}/usrs" ] || die "No \"users\" file under \"${path}\"."
+
+read -r -a users <<< "$(find ./users -mindepth 1 -maxdepth 1 -type f -exec basename {} \;)"
+[ "${#users[@]}" > "0" ] || die "No users specified in this repository. Aborting."
+
+read -r -a oldusers <<< $(awk -F: '$3>=1000&&$1!="nfsnobody"{print $1}' /etc/passwd | tr '\n' ' ')
+for u in "${oldusers[@]}"; do
+	[ ! -f "${path}/users/${u}" ] || continue
+	userdel -r "${u}" || true
+done
+
+for u in "${users[@]}"; do
+	useradd -m -U -G wheel "${user}"
+	mkdir ~"${u}/.ssh"
+	cat "${path}/users/${u}" > ~"${u}"/.ssh/authorized_keys
+	chown -R "${u}:${u}" ~"${u}/.ssh"
+	chmod 700 ~"${u}/.ssh"
+	chmod 600 ~"${u}/.ssh/authorized_keys"
+done
