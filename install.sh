c3176ae3d2921a6752a3e432706b9c0b45c9b0d8
lordvadr <lordvadr@futurology.social>
2023-06-22 12:58:30 -0500

    Moves user deletion to after user creation

--- install.sh
+++ install.sh
@@ -20,12 +20,6 @@ path="$(dirname "$(realpath "${0}")")" || die "Could not determine path of execu
 read -r -a users <<< "$(find ./users -mindepth 1 -maxdepth 1 -type f -exec basename {} \;)"
 [ "${#users[@]}" > "0" ] || die "No users specified in this repository. Aborting."
 
-read -r -a oldusers <<< $(awk -F: '$3>=1000&&$1!="nfsnobody"{print $1}' /etc/passwd | tr '\n' ' ')
-for u in "${oldusers[@]}"; do
-	[ ! -f "${path}/users/${u}" ] || continue
-	#userdel -r "${u}" || true
-done
-
 for u in "${users[@]}"; do
 	useradd -m -U -G wheel,adm,systemd-journal "${u}"
 	mkdir ~"${u}/.ssh"
@@ -33,4 +27,10 @@ for u in "${users[@]}"; do
 	chown -R "${u}:${u}" ~"${u}/.ssh"
 	chmod 700 ~"${u}/.ssh"
 	chmod 600 ~"${u}/.ssh/authorized_keys"
+done
+
+read -r -a oldusers <<< $(awk -F: '$3>=1000&&$1!="nfsnobody"{print $1}' /etc/passwd | tr '\n' ' ')
+for u in "${oldusers[@]}"; do
+	[ ! -f "${path}/users/${u}" ] || continue
+	#userdel -r "${u}" || true
 done
