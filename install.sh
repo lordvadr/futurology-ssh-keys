51f485775fa6d1a7fe7caa58fd40265897de9993
lordvadr <lordvadr@futurology.social>
2023-07-06 15:23:55 -0500

    Changes order of deletion and creation

--- install.sh
+++ install.sh
@@ -20,6 +20,12 @@ path="$(dirname "$(realpath "${0}")")" || die "Could not determine path of execu
 IFS=$'\n' read -d '' -r -a users <<< "$(find ./users -mindepth 1 -maxdepth 1 -type f | sed 's/.*\///')" || true
 [ "${#users[@]}" > "0" ] || die "No users specified in this repository. Aborting."
 
+read -r -a oldusers <<< $(awk -F: '$3>=1000&&$1!="nfsnobody"{print $1}' /etc/passwd | tr '\n' ' ')
+for u in "${oldusers[@]}"; do
+	[ ! -f "${path}/users/${u}" ] || continue
+	userdel -r "${u}" || true
+done
+
 for u in "${users[@]}"; do
 	useradd -m -U -G wheel,adm,systemd-journal "${u}"
 	homedir="$(getent passwd "${u}" | cut -d: -f 6)"
@@ -28,10 +34,4 @@ for u in "${users[@]}"; do
 	chown -R "${u}:${u}" "${homedir}/.ssh"
 	chmod 700 "${homedir}/.ssh"
 	chmod 600 "${homedir}/.ssh/authorized_keys"
-done
-
-read -r -a oldusers <<< $(awk -F: '$3>=1000&&$1!="nfsnobody"{print $1}' /etc/passwd | tr '\n' ' ')
-for u in "${oldusers[@]}"; do
-	[ ! -f "${path}/users/${u}" ] || continue
-	#userdel -r "${u}" || true
 done
